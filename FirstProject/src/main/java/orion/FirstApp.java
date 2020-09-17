8716536f987d99c6f4f5ae49558ef9ce0b323ab5
Lavigne Family <Lavigne Family@10.0.0.115>
2020-09-17 09:51:01 -0600

    Committing initial code

---
+++ FirstProject/src/main/java/orion/FirstApp.java
@@ -0,0 +1,16 @@
+package orion;
+
+public class FirstApp {
+
+	public FirstApp() {
+		// TODO Auto-generated constructor stub
+		System.out.println("hello world");
+		System.out.println("Daily Advice: Update your accounts");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new FirstApp();
+	}
+
+}
